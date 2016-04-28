37d9a336615ee81df2949e282afbfc204b849151
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-04-28 23:36:24 +0100

    Namespace the app and add some names for url patterns.

--- api_v0/urls.py
+++ api_v0/urls.py
@@ -10,11 +10,15 @@ from rest_framework.routers import DefaultRouter
 #not sure if this is needed, but I'll use it anyway.
 from rest_framework.urlpatterns import format_suffix_patterns
 
+from . import views 
+
+app_name = 'api_v0'
+
 #router auto-urls as well as a hardcoded pattern...
 urlpatterns = format_suffix_patterns([
-  url(r'scores/$', views.ScoresRowList.as_view()),
+  url(r'scores/$', views.ScoresRowList.as_view(), name='dummy-scores'),
   url(r'scores_row_list/$', views.scores_row_list),
-  url(r'one-scores/(?P<pk>[0-9]+)/$', views.OneScoresRow.as_view()),
+  url(r'one-scores/(?P<pk>[0-9]+)/$', views.OneScoresRow.as_view(), name='one-scores'),
   url(r'one-scores-snpid/rs(?P<snp>[0-9]+)/$', views.OneScoresRowSnp.as_view()), 
 ] )
 
