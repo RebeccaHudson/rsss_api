b244963e5b47df12e4da54b2c94efeeaf58e152b
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-02 18:03:26 +0100

    Change the url for search from scores_row_list to search.

--- api_v0/urls.py
+++ api_v0/urls.py
@@ -17,9 +17,7 @@ app_name = 'api_v0'
 #router auto-urls as well as a hardcoded pattern...
 urlpatterns = format_suffix_patterns([
   url(r'scores/$', views.ScoresRowList.as_view(), name='dummy-scores'),
-  url(r'scores_row_list/$', views.scores_row_list, name='search'),
+  url(r'search/$', views.scores_row_list, name='search'),
   url(r'one-scores/(?P<pk>[0-9]+)/$', views.OneScoresRow.as_view(), name='one-scores'),
   url(r'one-scores-snpid/rs(?P<snp>[0-9]+)/$', views.OneScoresRowSnp.as_view(), name = 'one-scores-snpid'),
 ] )
-
-
