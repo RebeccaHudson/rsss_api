71d7cfad389b617b842721aa01a44fa72769ced8
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-13 01:30:51 +0100

    Change the way that tests are organized such that there is a tests module where files containing tests are named test_BLANK.py. ALSO: you can just get everything for setting up the test db, comparing JSON test inputs to test outputs, and writing new test data from the file tests/painstaking_manual_test_setup.py; include that file and subclass RSSS_APITestCase, which itself subclasses APITestCase from the django REST Framework.

--- api_v0/tests/test_everything.py
+++ api_v0/tests/test_everything.py
@@ -2,69 +2,15 @@ from django.core.urlresolvers import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from api_v0.models import ScoresRow 
-
+from api_v0.tests.painstaking_manual_test_setup import RSSS_APITestCase
 from django.db import connection
 import json
 import os
 
 #TODO spilt this up into smaller, shorter files.
 
-class ScoresRowTests(APITestCase):
-
-    def setUp(self):
-      connection.cursor().db.set_rollback(False)  
-      c = connection.cursor()
-      c.execute('SHOW TABLES')
-      resp = c.fetchall() 
-      if (u'snp_scores_2',) in resp:
-        print "snp_scores_2 test table already setup.. skipping that setup"
-        return 
-      self.setup_table_in_testdb(c)
-      self.read_sql_into_testdb(c)
-            
-    def setup_table_in_testdb(self, cursor):
-      sql = """
-        CREATE TABLE snp_scores_2 (
-         id INTEGER PRIMARY KEY AUTO_INCREMENT,
-         snpid VARCHAR(20),  
-         motif VARCHAR(15),
-         motif_len INTEGER,
-         log_lik_ref FLOAT, log_lik_snp FLOAT, log_lik_ratio FLOAT,
-         log_enhance_odds FLOAT, log_reduce_odds FLOAT, 
-         ref_start INTEGER, snp_start INTEGER,
-         ref_end INTEGER, snp_end INTEGER,
-         ref_strand CHAR(1), snp_strand CHAR(1),
-         pval_ref FLOAT, pval_snp FLOAT, pval_cond_ref FLOAT, pval_cond_snp FLOAT,
-         pval_diff FLOAT, pval_rank FLOAT, chromosome VARCHAR(10), pos INTEGER);
-         """
-      cursor.execute(sql)
-
-    def read_sql_into_testdb(self, cursor):
-        sql_input = os.path.join(os.path.dirname(__file__), 
-                                'sql_test_data', 'sql_out_test_data-2.sql')
-        with open(sql_input, 'r') as f:
-          for line in f:
-            cursor.execute(line)
-
-    #writing up new test data after every migration is a real mother.
-    #Check sufficiently what this writes out to avoid automating error propogation. 
-    def write_response_to_appropriate_testfile(self, response_data, name_of_expected_output_file):
-      with open (os.path.join ( os.path.dirname(__file__),
-                                 'test_outputs',
-                                 name_of_expected_output_file  + '.json'),
-                                 'w') as data_file:
-        json.dump(response_data, data_file) 
-        print("just dumped out the following:\n" + str(response_data))
-
- 
-    def compare_response(self, response_data, name_of_expected_output_file):
-      with open (os.path.join( os.path.dirname(__file__), 
-                               'test_outputs',
-                                name_of_expected_output_file + ".json"),
-                 'r') as data_file:
-          expected_output = json.load(data_file)
-      self.assertEqual(response_data, expected_output)
-    
+#class ScoresRowTests(APITestCase):
+class ScoresRowTests(RSSS_APITestCase): #idea is that this will inheit from API TestCase   
     def test_retrieve_one_row_by_id(self):
       url = reverse('api_v0:one-scores', args=(23,))
       response = self.client.get(url) 
