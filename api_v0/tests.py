65eae244c28c247dcb1642b28927bcdce603c384
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-18 00:33:44 +0100

    Begin to expose data with cassandra; try to avoid going too far before merging up to where we are this week.

--- api_v0/tests.py
+++ api_v0/tests.py
@@ -4,41 +4,49 @@ from rest_framework.test import APITestCase
 from api_v0.models import ScoresRow 
 
 from django.db import connection
+import django_cassandra_engine
 import json
 import os
 
 #TODO spilt this up into smaller, shorter files.
-
+#TODO parameterize the names of tables and databases, use the stuff in settings/models?
 class ScoresRowTests(APITestCase):
 
     def setUp(self):
       connection.cursor().db.set_rollback(False)  
+       
       c = connection.cursor()
-      c.execute('SHOW TABLES')
-      resp = c.fetchall() 
-      if (u'snp_scores_1',) in resp:
-        print "snp_scores_1 test table already setup.. skipping that setup"
+      insa = c.db.connection.cluster.metadata.keyspaces['test_rsnp_data'].tables
+      if c.db.connection.cluster.metadata.keyspaces.has_key('test_rsnp_data'):
+        print("There is a test database 'keyspace' called test_rsnp_data alreadly setup.")
+      print(dir(insa))
+      print(str(insa))
+
+      if u'snp_scores_2' in c.db.connection.cluster.metadata.keyspaces['test_rsnp_data'].tables.keys():
+        print "snp_scores_2 test table already setup.. skipping that setup"
         return 
       self.setup_table_in_testdb(c)
-      self.read_sql_into_testdb(c)
+      self.read_cql_into_testdb(c)
             
     def setup_table_in_testdb(self, cursor):
-      sql = """CREATE TABLE snp_scores_1 (
-           id INTEGER PRIMARY KEY AUTO_INCREMENT,
-           snpid VARCHAR(20),  
-           motif VARCHAR(15),
-           motif_len INTEGER,
-           log_lik_ref FLOAT, log_lik_snp FLOAT, log_lik_ratio FLOAT,
-           log_enhance_odds FLOAT, log_reduce_odds FLOAT, 
-           ref_start INTEGER, snp_start INTEGER,
-           ref_end INTEGER, snp_end INTEGER,
-           ref_strand CHAR(1), snp_strand CHAR(1)
-         );""" 
-      cursor.execute(sql)
+      cql="""CREATE TABLE snp_scores_2 (
+         snpid VARCHAR,  
+         motif VARCHAR,
+         motif_len INT,
+         log_lik_ref FLOAT, log_lik_snp FLOAT, log_lik_ratio FLOAT,
+         log_enhance_odds FLOAT, log_reduce_odds FLOAT, 
+         ref_start INT, snp_start INT,
+         ref_end INT, snp_end INT,
+         ref_strand VARCHAR, snp_strand VARCHAR,
+         pval_ref FLOAT, pval_snp FLOAT, pval_cond_ref FLOAT, pval_cond_snp FLOAT,
+         pval_diff FLOAT, pval_rank FLOAT, chr VARCHAR, pos INT,
+         PRIMARY KEY( (snpid), chr, pos, motif, pval_rank)
+         );"""
+      cursor.execute(cql)
 
-    def read_sql_into_testdb(self, cursor):
+    def read_cql_into_testdb(self, cursor):
         sql_input = os.path.join(os.path.dirname(__file__), 
-                                'sql_test_data', 'sql_out_test_data-0.sql')
+                                'cql_test_data', 'cql_out_test_data-0.cql')
         with open(sql_input, 'r') as f:
           for line in f:
             cursor.execute(line)
@@ -50,19 +58,23 @@ class ScoresRowTests(APITestCase):
                  'r') as data_file:
           expected_output = json.load(data_file)
       self.assertEqual(response_data, expected_output)
-    
-    def test_retrieve_one_row_by_id(self):
-      url = reverse('api_v0:one-scores', args=(23,))
-      response = self.client.get(url) 
-      self.compare_response(response.data, 'test_retrieve_one_row')
-      self.assertEqual(response.status_code, status.HTTP_200_OK)
+   
+    # There is not any way to do this with the cassandra data model. 
+    #def test_retrieve_one_row_by_id(self):
+    #  url = reverse('api_v0:one-scores', args=(23,))
+    #  response = self.client.get(url) 
+    #  self.compare_response(response.data, 'test_retrieve_one_row')
+    #  self.assertEqual(response.status_code, status.HTTP_200_OK)
     
     def test_retrieve_one_row_by_snpid(self):
       #the line below returns the snp: 'rs561784591'
-      url = reverse('api_v0:one-scores-snpid', args=(561784591,))
+      url = reverse('api_v0:one-scores-snpid', args=(201336010,))
       response = self.client.get(url) 
       #an ordered dict comes out of the .filter call and the .data is an
-      self.compare_response(json.loads(response.content), 'test_retrieve_row_by_snpid')
+      jr = json.loads(response.content) 
+      self.assertEqual(len(jr), 1)
+      self.assertEqual(len(jr[0].keys()), 21)
+      #self.compare_response(json.loads(response.content), 'test_retrieve_row_by_snpid')
       self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_nomatch_response_for_retrieve_one_row_by_snpid(self):
