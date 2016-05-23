302ec8bc37e7ee63a3c6c39131cc83cf4721db26
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-23 16:59:09 +0100

    Completely rewrite all of the queries to use the Cassandra tables.

--- api_v0/views.py
+++ api_v0/views.py
@@ -25,7 +25,6 @@ from django.conf import settings
 
 from rest_framework import status 
 
-SCORES_TABLE_NAME='snp_scores_2'
 
 #this works, leave it as an example...
 class ScoresRowList(APIView):
@@ -53,13 +52,13 @@ class OneScoresRow(APIView):
     serializer = ScoresRowSerializer(scores_row)
     return Response(serializer.data) 
 
-
+#deprecated for production; p-value cutoff not added.
 class OneScoresRowSnp(APIView):
   def get(self, request, snp, format = None):
     rsnp = 'rs' + str(snp)
     #scores_rows = ScoresRow.objects.filter(snpid=rsnp)
     cursor = connection.cursor()
-    scores_rows = cursor.execute('SELECT * from ' + SCORES_TABLE_NAME +' where snpid = ' + repr(rsnp) )
+    scores_rows = cursor.execute('SELECT * from '+settings.CASSANDRA_TABLE_NAMES['TABLE_FOR_SNPID_QUERY']+' where snpid = '+repr(rsnp) )
     scores_rows = scores_rows.current_rows
     if len(scores_rows) == 0:
       return Response('No data for that SNPid',
@@ -75,11 +74,19 @@ def scores_row_list(request):
     print str(request.data)  #expect this to be a list of quoted strings...
     scoresrows_to_return = []
     cursor = connection.cursor()  # yep, manual SQL neded here too.
+
+    #pval_rank = request.data['pvalue_rank']  # this is what we'll call it when it comes in from the web.
+    pval_rank = 0.05
+
     for one_snpid in request.data:
       #scoresrows = ScoresRow.objects.filter(snpid=one_snpid)
-      cql = 'SELECT * from ' + SCORES_TABLE_NAME + \
-            ' where snpid = ' + repr(one_snpid.encode('ascii')) 
-      #print("CQL: " + cql)
+      cql = 'SELECT * from ' +                                       \
+            settings.CASSANDRA_TABLE_NAMES['TABLE_FOR_SNPID_QUERY']+ \
+            ' where snpid = ' + repr(one_snpid.encode('ascii'))    + \
+            ' and pval_rank < ' + str(pval_rank) + ' ALLOW FILTERING;'
+      print("CQL: " + cql)
+      # example: :select snpid, chr, pos from snp_scores_3 where snpid = 'rs757310325' and pval_rank < 0.05 allow filtering;
+     
       scoresrows = cursor.execute(cql).current_rows
                                    
       for matching_row_of_data in scoresrows:
@@ -102,22 +109,41 @@ def search_by_genomic_location(request):
   start_pos = request.data['start_pos']
   end_pos = request.data['end_pos']
   chromosome = request.data['chromosome']  # TODO: check for invlaid chromosome
+  #pval_rank = request.data['pvalue_rank']  # this is what we'll call it when it comes in from the web.
+  pval_rank = 0.05
+
 
   if end_pos < start_pos:
     return Response('Start position is less than end position.',
                     status = status.HTTP_400_BAD_REQUEST)
-
+ 
   if end_pos - start_pos > settings.HARD_LIMITS['MAX_BASES_IN_GL_REQUEST']:
     return Response('Requested region is too large', 
                      status=status.HTTP_400_BAD_REQUEST)
-   
-  #refer to: https://docs.djangoproject.com/en/1.9/ref/models/querysets/#id4 
-  #scoresrows = ScoresRow.objects.filter(chromosome=chromosome, 
-  #                                      pos__gte=start_pos, pos__lte=end_pos) 
-  cql = 'SELECT * from ' + SCORES_TABLE_NAME               + \
-        ' where chr = ' + repr(chromosome.encode('ascii')) + \
-        ' and pos >= ' + str(start_pos)                    + \
-        ' and pos <= ' + str(end_pos) + ' ALLOW FILTERING'
+   #refer to: https://docs.djangoproject.com/en/1.9/ref/models/querysets/#id4 
+   #scoresrows = ScoresRow.objects.filter(chromosome=chromosome, 
+   #                                      pos__gte=start_pos, pos__lte=end_pos) 
+   # cql='SELECT * from ' +                                   \
+   #     settings.CASSANDRA_TABLE_NAMES['TABLE_FOR_GL_REGION_QUERY'] + \
+   #     ' where chr = ' + repr(chromosome.encode('ascii')) + \
+   #     ' and pos >= ' + str(start_pos)                    + \
+   #     ' and pos <= ' + str(end_pos) + ' ALLOW FILTERING'
+
+   #not the right search...
+   #cql = 'select * from ' +                                             \
+   #       settings.CASSANDRA_TABLE_NAMES['TABLE_FOR_GL_REGION_QUERY'] + \
+   #       where snpid = 'rs757310325' and pval_rank < 0.05 allow filtering;
+
+   #'{:06.2f}'.format(3.141592653589793)
+  cql = ' select * from '        +                                               \
+        settings.CASSANDRA_TABLE_NAMES['TABLE_FOR_GL_REGION_QUERY']            + \
+        ' where chr = ' + repr(chromosome.encode('ascii'))                     + \
+        ' and (pos, pval_rank) > ('+ str(start_pos) + ',' + str(0)        +')' + \
+        ' and (pos, pval_rank) < ('+ str(end_pos)   + ',' + str(pval_rank)+')' + \
+        ' ALLOW FILTERING;' 
+          #consider adding a HARD LIMIT for general use.
+          #does this actually work?
+
   cursor = connection.cursor()
   scoresrows = cursor.execute(cql).current_rows  
 
