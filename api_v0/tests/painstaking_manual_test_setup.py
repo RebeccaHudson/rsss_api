fb22f3b77300fc7dbece7fcac259668edfaaadb6
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-23 16:53:17 +0100

    Add code to setup BOTH tables of scoresrows manually before testing, and to just bail out of the test setup if one of the test tables is already present in the test database.

--- api_v0/tests/painstaking_manual_test_setup.py
+++ api_v0/tests/painstaking_manual_test_setup.py
@@ -3,7 +3,8 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from api_v0.models import ScoresRow 
 
-from django.db import connection
+from django.db     import connection
+from django.conf   import settings
 import json
 import os
 
@@ -13,13 +14,21 @@ class RSSS_APITestCase(APITestCase):
     def setUp(self):
       connection.cursor().db.set_rollback(False)  
       c = connection.cursor()
-      if u'snp_scores_2' in c.db.connection.cluster.metadata.keyspaces['test_rsnp_data'].tables.keys():
-        print "snp_scores_2 test table already setup.. skipping that setup"
+      tbl = settings.CASSANDRA_TABLE_NAMES['TABLE_FOR_SNPID_QUERY'] 
+      if tbl in c.db.connection.cluster.metadata.keyspaces['test_rsnp_data'].tables.keys():
+        print "test tables already setup.. skipping that setup"
         return 
-      self.setup_table_in_testdb(c)
-      self.read_cql_into_testdb(c)
+      self.setup_tables_for_testdb(c)
+      #self.setup_table_in_testdb(c)
+      #self.read_cql_into_testdb(c)
 
+    # each of these functions is responsible for loading its own data
+    # (all of this data should be identical) 
+    def setup_tables_for_testdb(self, cursor):
+       self.setup_table_for_search_by_snpid_in_testdb(cursor)
+       self.setup_table_for_search_by_gl_in_testdb(cursor)
 
+    # TODO: Deprecated?
     def setup_table_in_testdb(self, cursor):
       cql="""CREATE TABLE snp_scores_2 (
          snpid VARCHAR,  
@@ -37,9 +46,48 @@ class RSSS_APITestCase(APITestCase):
       cursor.execute(cql)
 
 
-    def read_cql_into_testdb(self, cursor):
+
+    def setup_table_for_search_by_snpid_in_testdb(self, cursor):
+      tbl = settings.CASSANDRA_TABLE_NAMES['TABLE_FOR_SNPID_QUERY'] 
+      cql="""CREATE TABLE """ + tbl + """ (
+         snpid VARCHAR,
+         motif VARCHAR,
+         motif_len INT,
+         log_lik_ref FLOAT, log_lik_snp FLOAT, log_lik_ratio FLOAT,
+         log_enhance_odds FLOAT, log_reduce_odds FLOAT,
+         ref_start INT, snp_start INT,
+         ref_end INT, snp_end INT,
+         ref_strand VARCHAR, snp_strand VARCHAR,
+         pval_ref FLOAT, pval_snp FLOAT, pval_cond_ref FLOAT, pval_cond_snp FLOAT,
+         pval_diff FLOAT, pval_rank FLOAT, chr VARCHAR, pos INT,
+         PRIMARY KEY( (snpid), pval_rank, chr, pos, motif)
+         );"""
+      cursor.execute(cql)
+      self.read_cql_into_testdb(cursor, 'cql-data-3.txt')
+
+
+    def setup_table_for_search_by_gl_in_testdb(self, cursor):
+      tbl = settings.CASSANDRA_TABLE_NAMES['TABLE_FOR_GL_REGION_QUERY'] 
+      cql="""CREATE TABLE """ + tbl + """ (
+         snpid VARCHAR,
+         motif VARCHAR,
+         motif_len INT,
+         log_lik_ref FLOAT, log_lik_snp FLOAT, log_lik_ratio FLOAT,
+         log_enhance_odds FLOAT, log_reduce_odds FLOAT,
+         ref_start INT, snp_start INT,
+         ref_end INT, snp_end INT,
+         ref_strand VARCHAR, snp_strand VARCHAR,
+         pval_ref FLOAT, pval_snp FLOAT, pval_cond_ref FLOAT, pval_cond_snp FLOAT,
+         pval_diff FLOAT, pval_rank FLOAT, chr VARCHAR, pos INT,
+         PRIMARY KEY( (snpid), chr, pos, pval_rank)
+         ); """
+      cursor.execute(cql)
+      self.read_cql_into_testdb(cursor, 'cql-data-4.txt')
+
+
+    def read_cql_into_testdb(self, cursor, name_of_testdata_file):
         sql_input = os.path.join(os.path.dirname(os.path.dirname(__file__)), 
-                                'cql_test_data', 'cql_out_test_data-0.cql')
+                                'cql_test_data', name_of_testdata_file)
         with open(sql_input, 'r') as f:
           for line in f:
             cursor.execute(line)
@@ -71,7 +119,7 @@ class RSSS_APITestCase(APITestCase):
     # TODO: check that all of the expected fields have the expected names.
     def check_that_response_is_well_formed(self, response_data, expected_count):
       self.assertEqual(len(response_data), expected_count)
-      expected_attrs_in_datarow = 21  #how many fields to expect 
+      expected_attrs_in_datarow = 22  #how many fields to expect 
       for one_item in response_data:      
         self.assertEqual(len(one_item.keys()), expected_attrs_in_datarow)
       print("Response: " + str(response_data) + " is well-formed.") 
