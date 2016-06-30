25669500efc807bd1d2263e03676a369196a9c61
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-06-30 23:55:36 +0000

    Add staticfiles path in vain, as I don't know how to finish unhooking the proxy stuff in nginx.

--- rsss_api/settings.py
+++ rsss_api/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = os.path.join(BASE_DIR, "static/")
+
+
 
 DEFAULT_P_VALUE = 0.05
 
