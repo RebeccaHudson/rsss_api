2e78787b0ccb2f5b0beacac4a1f92237b8ebb510
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-01-17 22:30:25 +0000

    Remove the un-refactored versions of methods for each search type.

--- api_v0/views.py
+++ api_v0/views.py
@@ -157,34 +157,6 @@ def setup_es_url(data_type, url_base, operation="_search",
          url = url + "&from=" + str(from_result) 
      return url
 
-@api_view(['POST'])
-def scores_row_list(request):
-    #print str(request.data)  #expect this to be a list of quoted strings...
-    list_of_data_to_return = []
-
-    pval_rank = get_p_value(request) 
-    snpid_list = request.data['snpid_list']
-    #chunked_snpid_list = chunk(snpid_list, 50) #TODO: parameterize chunk size somehow.
-    from_result = request.data.get('from_result')
-    page_size = request.data.get('page_size')
-
-    es_query = prepare_snpid_search_query_from_snpid_chunk(snpid_list, pval_rank)  
-    es_url =prepare_es_url('atsnp_output',
-                                from_result=from_result,
-                                page_size=page_size)
-    if es_url is None:
-        return Response('Elasticsearch is down, please contact admins.', 
-                         status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    try:
-        es_result = requests.post(es_url ,  data=es_query, timeout=100)
-        print "Successful search, without timeout that is."
-    except requests.exceptions.Timeout:
-        return Response('Elasticsearch timed out. Contact admins.',
-                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    data_back = get_data_out_of_es_result(es_result)
-    return return_any_hits(data_back)
-
 
 #a refactor of scrores_row_list
 @api_view(['POST'])
@@ -218,28 +190,6 @@ def check_and_aggregate_gl_search_params(request):
                        status=status.HTTP_400_BAD_REQUEST)
     return gl_coords
 
-#def prepare_json_for_gl_query(gl_coords, pval_rank):
-#    pvalue_filter = prepare_json_for_pvalue_filter(pval_rank)
-#    sort = prepare_json_for_sort()
-#    j_dict = {   
-#        "sort" : sort["sort"], 
-#        "query":
-#        {
-#            "bool" : {
-#                "must" : [
-#                   {
-#                     "range": {
-#                          "pos" : {  "from" : gl_coords['start_pos'], "to" : gl_coords['end_pos'] }
-#                      }
-#                   },
-#                   { "term" : { "chr" : gl_coords['chromosome'] } }
-#                ],
-#                "filter":  pvalue_filter["filter"]
-#            }
-#        }
-#    }
-#    json_out = json.dumps(j_dict)
-#    return json_out
 
 #try to use 'filter' queries to speed this up.
 def prepare_json_for_gl_query(gl_coords, pval_rank):
@@ -280,39 +230,6 @@ def return_any_hits(data_returned):
     print "called return any hits..."
     return Response(data_returned, status=status.HTTP_200_OK)
 
-@api_view(['POST'])
-def search_by_genomic_location(request):
-    
-    gl_chunk_size = 100   # TODO: parametrize chunk size in the settings file.
-    gl_coords_or_error_response = check_and_aggregate_gl_search_params(request)
-
-    if not gl_coords_or_error_response.__class__.__name__  == 'dict':
-        return gl_coords_or_error_response 
-
-    pvalue = get_p_value(request) #use a default if an invalid value is requested.
-
-    gl_coords = gl_coords_or_error_response
-    from_result = request.data.get('from_result')
-    page_size = request.data.get('page_size')
-
-    # The following code was copied into the bottom of search_by_snpid_window
-    # TODO: consider DRYing up this part of the code
-    es_query = prepare_json_for_gl_query(gl_coords, pvalue)
-    es_url =  prepare_es_url('atsnp_output', 
-                            from_result=from_result,
-                            page_size=page_size)
-    if es_url is None: 
-        return Response('Elasticsearch is down, please contact admins.', 
-                         status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    try:
-        es_result = requests.post( es_url , data=es_query, timeout=100)
-    except requests.exceptions.Timeout:
-        return Response('Elasticsearch timed out. Contact admins.',
-                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
-    data_back = get_data_out_of_es_result(es_result)
-    return return_any_hits(data_back)
-
 
 #refactor this one first.
 @api_view(['POST'])
@@ -336,14 +253,6 @@ def alternate_search_by_genomic_location(request):
 
     #url stuff is handled in query_elasticsearch.
     return query_elasticsearch(es_query, es_params)
-
-
-
-
-
-
-
-
 
 
 #TODO: factor this out into a 'helper' file, it's not strictly a view.
@@ -438,40 +347,6 @@ def prepare_json_for_encode_tf_query(encode_prefix, pval_rank):
 #  Web interface translates motifs to transcription factors and vice-versa
 #  this API expects motif values. 
 @api_view(['POST'])
-def search_by_trans_factor(request):
-
-    from_result = request.data.get('from_result')
-    page_size = request.data.get('page_size')
-    pvalue = get_p_value(request)
-    es_url = prepare_es_url('atsnp_output', from_result=from_result, page_size=page_size)
-
-    if es_url is None:
-        return Response('Elasticsearch is down, please contact admins.', 
-                         status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-  
-    if  request.data.get('tf_library') == 'encode':
-        return search_by_encode_trans_factor(request, es_url, pvalue)
-    #otherwise, go with the == 'jaspar' behavior previously implemented.
-
-    motif_or_error_response = check_and_return_motif_value(request)
-    if not type(motif_or_error_response) == list:
-        return motif_or_error_response   #it's an error response    
-
-    motif_list = motif_or_error_response       # above established this is a motif. 
-    es_query = prepare_json_for_tf_query(motif_list, pvalue)
-
-    try:
-        es_result = requests.post(es_url, data=es_query, timeout=100)
-    except requests.exceptions.Timeout:
-        return Response('Elasticsearch timed out. Contact admins.',
-                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    data_back = get_data_out_of_es_result(es_result)
-    return return_any_hits(data_back)
-
-
-#  Web interface translates motifs to transcription factors and vice-versa
-#  this API expects motif values. 
-@api_view(['POST'])
 def alternate_search_by_trans_factor(request):
     pvalue = get_p_value(request)
 
@@ -531,50 +406,7 @@ def get_position_of_gene_by_name(gene_name):
     return gl_coords 
      
 
-@api_view(['POST'])
-def search_by_gene_name(request):
-    gene_name = request.data.get('gene_name')
-    window_size = request.data.get('window_size')
-    pvalue = get_p_value(request)
 
-    if window_size is None:
-        window_size = 0
-
-    if gene_name is None:
-        return Response('No gene name specified.', 
-                        status = status.HTTP_400_BAD_REQUEST)
-    page_size =  request.data.get('page_size')
-    from_result =  request.data.get('from_result')
-    es_url = prepare_es_url('atsnp_output', 
-                            from_result=from_result,
-                            page_size=page_size)
-    #if es is down, find out now and report the error from here
-    if es_url is None:
-        return Response('Elasticsearch is down, please contact admins.', 
-                         status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-    gl_coords = get_position_of_gene_by_name(gene_name)
-    if gl_coords is None: 
-        return Response('Gene name not found in database.', 
-                        status = status.HTTP_400_BAD_REQUEST)
-    #print "continued gene name search after Respnose.."
-    gl_coords['start_pos'] = int(gl_coords['start_pos']) - window_size
-    gl_coords['end_pos'] = int(gl_coords['end_pos']) + window_size
-
-    es_query = prepare_json_for_gl_query(gl_coords, pvalue)
-    #print "es query for gene name search " + es_query
-   
-    try:
-        es_result = requests.post( es_url, data=es_query, timeout=100)
-    except requests.exceptions.Timeout:
-        return Response('Elasticsearch timed out. Contact admins.',
-                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    data_back  = get_data_out_of_es_result(es_result)
-    return return_any_hits(data_back)
-
-#TODO: test this properly.
 @api_view(['POST'])
 def alternate_search_by_gene_name(request):
     gene_name = request.data.get('gene_name')
@@ -604,66 +436,6 @@ def alternate_search_by_gene_name(request):
     es_query = prepare_json_for_gl_query(gl_coords, pvalue)
     return query_elasticsearch(es_query, es_params)
 
-@api_view(['POST'])
-def search_by_window_around_snpid(request):
-    one_snpid = request.data.get('snpid')
-    window_size = request.data.get('window_size')
-    pvalue = get_p_value(request)
-
-    page_size = request.data.get('page_size')
-    from_result = request.data.get('from_result')
-
-    if window_size is None:
-        window_size = 0
- 
-    if one_snpid is None: 
-        return Response('No snpid specified.', 
-                         status = status.HTTP_400_BAD_REQUEST)
-
-    #This URL is for looking up the genomic location of a SNPid. 
-    #has to be re-prepared for pageable search.
-    es_url = prepare_es_url('atsnp_output') 
-
-    #if elasticsearch is down, find out now. 
-    if es_url is None:
-        return Response('Elasticsearch is down, please contact admins.', 
-                         status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    query_for_snpid_location = {"query":{"match":{"snpid":one_snpid }}}
-    es_query = json.dumps(query_for_snpid_location)
-    es_result = requests.post(es_url, data=es_query, timeout=100)
-    records_for_snpid = get_data_out_of_es_result(es_result)
-
-    if len(records_for_snpid['data']) == 0: 
-        return Response('No data for snpid ' + one_snpid + '.', 
-                        status = status.HTTP_204_NO_CONTENT)
-
-    record_to_pick = records_for_snpid['data'][0]
-    gl_coords = { 'chromosome' :  record_to_pick['chr'],
-                  'start_pos'  :  record_to_pick['pos'] - window_size,
-                  'end_pos'    :  record_to_pick['pos'] + window_size
-                 }
-    if gl_coords['start_pos'] < 0:
-        #: TODO consider adding a warning here if this happens?
-        gl_coords['start_pos'] = 0
-
-    es_query = prepare_json_for_gl_query(gl_coords, pvalue)
-    print "es query for snpid window search " + es_query
-   
-    #This probably isn't the right place to put the from result
-    es_url = prepare_es_url('atsnp_output', 
-                            from_result=from_result,
-                            page_size=page_size) 
-    try:
-        es_result = requests.post(es_url, data=es_query, timeout=100)
-    except requests.exceptions.Timeout:
-        return Response('Elasticsearch timed out. Contact admins.',
-                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    data_back = get_data_out_of_es_result(es_result)
-    return return_any_hits(data_back)
-
-# TODO: revive the plotting code!!
 
 @api_view(['POST'])
 def alternate_search_by_window_around_snpid(request):
