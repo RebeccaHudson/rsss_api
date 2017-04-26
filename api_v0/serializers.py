bc6fae39e70a8fa64aae8f29d8b5e35ac1633bed
Rebecca Hudson <rhudson@atsnp.biostat.wisc.edu>
2017-04-26 23:23:24 +0000

    Remove fully dropped fields from the ScoresRow serializer.

--- api_v0/serializers.py
+++ api_v0/serializers.py
@@ -3,14 +3,15 @@ from api_v0.models import ScoresRow
 
 #for whatever reason, this does NOT want to be a ListSerializer, 
 #I get info that says 'child is a required element'
+
+# dropped fields:       'ref_start', 'snp_start', 'ref_end', 'snp_end',
 class ScoresRowSerializer(serializers.ModelSerializer):
   class Meta:
     model = ScoresRow
     fields = ('chr', 'pos', 
-              'snpid', 'motif', 'motif_len', 
+              'snpid', 'motif',  
               'log_lik_ref', 'log_lik_snp', 'log_lik_ratio', 
               'log_enhance_odds', 'log_reduce_odds',
-              'ref_start', 'snp_start', 'ref_end', 'snp_end',
               'ref_strand', 'snp_strand',
               'pval_ref', 'pval_snp',
               'pval_cond_ref', 'pval_cond_snp', 
