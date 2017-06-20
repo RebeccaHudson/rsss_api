c2908bcd5d5b80f36350f2b76088d693cf36c8ee
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-06-20 00:16:31 +0000

    Look for a flag that indicates whether this is a download in the search request, if it is, do not append motif plotting bits.

--- api_v0/views.py
+++ api_v0/views.py
@@ -26,32 +26,39 @@ from SnpidQuery import SnpidQuery
 
 from ElasticsearchURL import ElasticsearchURL
 
+
+
+
 #pull the _id field here; put it in with the rest of the data.
-def get_data_out_of_es_result(es_result):
+def get_data_out_of_es_result(es_result, pull_motifs):
     es_data = es_result.json()
     #print("es result : " + str(es_data))
     #print "es ruesult keys: "  + str(es_data.keys())
     if 'hits' in es_data.keys():
+        motifs_pulled = {}
         data =  [ x['_source'] for x in es_data['hits']['hits'] ]
         data_w_id = []
         for one_hit in es_data['hits']['hits']:
             one_hit_data = one_hit['_source']
             one_hit_data['id'] = one_hit['_id']
             if 'ref_and_snp_strand' in one_hit_data:
-                one_hit_data = DataReconstructor(one_hit_data).get_reconstructed_record()
+                one_hit_data = \
+                  DataReconstructor(one_hit_data).get_reconstructed_record()
             data_w_id.append(one_hit_data) 
-        #print "data w/ id " + repr(data_w_id)
+            if pull_motifs:
+                one_hit_data['motif_bits'] = \
+                   grab_plotting_data_for_one_motif(one_hit['_id'], motifs_pulled )
+                   #if pull_motifs is true, pull the motifs.
+            else:
+                one_hit_data['motif_bits'] = json.dumps({}) 
         hitcount = es_data['hits']['total']
-        #try this? data['_id'] = es_data[' 
-        #how should I include the _id field?
-        #print "data : " + repr(data)
         return { 'data':data_w_id, 'hitcount': hitcount}
     else:
-        print "no hits, then what is it? "
+        print "no hits..."
         print "es result : " + str(es_data)
     return { 'data' : None, 'hitcount': 0 } 
 
-def return_any_hits(data_returned):
+def return_any_hits(data_returned, pull_motifs):
     if data_returned['hitcount'] == 0:
         return Response('No matches.', status=status.HTTP_204_NO_CONTENT)
     if len(data_returned['data']) == 0:
@@ -59,6 +66,7 @@ def return_any_hits(data_returned):
                       str(data_returned['hitcount']) + 'results.',
                       status=status.HTTP_204_NO_CONTENT)
 
+    
     serializer = ScoresRowSerializer(data_returned['data'], many = True)
     data_returned['data'] = serializer.data
     #print "called return any hits..."
@@ -74,7 +82,7 @@ def setup_paging_parameters(request):
 #TODO: factor this out into a 'helper' file, it's not strictly a view.
 #es_params is a dict with from_result and page_size
 #TODO: add a standard parametrized elasticsearch timeout from settings.
-def query_elasticsearch(completed_query, es_params):
+def query_elasticsearch(completed_query, es_params, pull_motifs):
     search_url = ElasticsearchURL('atsnp_output', 
                                   from_result = es_params['from_result'], 
                                   page_size = es_params['page_size']).get_url()
@@ -87,8 +95,8 @@ def query_elasticsearch(completed_query, es_params):
     except requests.exceptions.ConnectionError:
         print "machine at " + esNode + " refused connection." 
     else: 
-        data_back = get_data_out_of_es_result(es_result) 
-        return return_any_hits(data_back)
+        data_back = get_data_out_of_es_result(es_result, pull_motifs) 
+        return return_any_hits(data_back, pull_motifs)
    #Algorithm to save a few requests here and there is used for the deatil view.
 
 def setup_and_run_query(request, query_class ):
@@ -100,8 +108,15 @@ def setup_and_run_query(request, query_class ):
     except NoDataFoundError as e:
         print "Responding a No Data Found error: " + e.message
         return Response(e.message, status=status.HTTP_204_NO_CONTENT)
-    es_params = setup_paging_parameters(request) 
-    return query_elasticsearch(es_query, es_params)
+    es_params = setup_paging_parameters(request)  
+    pull_motifs = check_for_download(request) #if True, get motif plotting data.
+    return query_elasticsearch(es_query, es_params, pull_motifs)
+
+def check_for_download(request):
+    #print "dir(request)" + repr(dir(request))
+    if 'for_download' in request.data and request.data['for_download']:
+        return False 
+    return True 
 
 @api_view(['POST'])
 def search_by_trans_factor(request):
@@ -124,7 +139,7 @@ def search_by_snpid(request):
 def search_by_genomic_location(request):
     return setup_and_run_query(request, GenomicLocationQuery)
 
-def get_one_item_from_elasticsearch_by_id(id_of_item):
+def get_one_item_from_elasticsearch_by_id(index_name, doc_type, id_of_item):
     #copied verbatim from the above method; should be refactored.
     machinesToTry = settings.ELASTICSEARCH_URLS[:]      
     random.shuffle(machinesToTry)   #try machines in different order. 
@@ -141,7 +156,8 @@ def get_one_item_from_elasticsearch_by_id(id_of_item):
         try: 
             #queries for single datum details use elasticsearch's GET API.
             url_base = esNode
-            search_url = url_base + "/atsnp_reduced_test/atsnp_output/" + id_of_item
+            #search_url = url_base + "/atsnp_reduced_test/atsnp_output/" + id_of_item
+            search_url = '/'.join([url_base, index_name, doc_type, id_of_item])
             #print "querying single item for detail view with url : " + search_url
             es_result = requests.get(search_url, timeout=100)
         except requests.exceptions.Timeout:
@@ -151,16 +167,48 @@ def get_one_item_from_elasticsearch_by_id(id_of_item):
         else: 
             return es_result
 
+
+#Motif is pulled from doc_id, if the motif to retireve is already present in
+#the 'motifs pulled' dict, it will be retrieved from there, not from Elastic.
+def grab_plotting_data_for_one_motif(for_doc_id, motifs_pulled=None):
+    which_motif = for_doc_id.split('_rs')[0]
+    if motifs_pulled is not None and which_motif in motifs_pulled:
+        motif_data = motifs_pulled[which_motif]
+    else: 
+        motif_data = \
+           get_one_item_from_elasticsearch_by_id('motif_plotting_data', 
+                                                 'motif_bits', which_motif)
+        motif_data = motif_data.json()
+        motif_data = motif_data['_source']
+        my_dict = motif_data
+        motif_data = \
+          {  str(k):(str(v) if             \
+             isinstance(v, unicode) else v)\
+             for k,v in my_dict.items()
+          }
+        motif_data = json.dumps(motif_data['plotting_bits'])
+    
+        if motifs_pulled is not None: 
+            motifs_pulled['which_motif'] = motif_data
+    return motif_data
+
+#Motif bits should ALWAYS come with the detail view. 
 @api_view(['POST'])
 def details_for_one(request):
     id_to_get_data_for = request.data.get('id_string')
-    #print "querying details for ID =  " + id_to_get_data_for
-    data_returned = get_one_item_from_elasticsearch_by_id(id_to_get_data_for)
+    
+    data_returned =\
+     get_one_item_from_elasticsearch_by_id(
+        'atsnp_reduced_test', 'atsnp_output', id_to_get_data_for)
+
     data_returned = data_returned.json()
-   
+    #print "these methods avaliable : " + repr(dir(data_returned))
     single_hit = data_returned['_source']    
     single_hit['id'] = data_returned['_id'] 
     es_result = DataReconstructor(single_hit).get_reconstructed_record()
+
+    es_result['motif_bits'] = \
+          grab_plotting_data_for_one_motif(id_to_get_data_for)
 
     #print "data_returned  " + repr(es_result)
     serializer = ScoresRowSerializer(es_result, many = False)
