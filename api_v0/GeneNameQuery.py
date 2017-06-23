2324d52547a029b37bc4ea9855112375facbfa23
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-06-23 18:35:33 +0000

    Include the Exceptions if they are to be defined at all.

--- api_v0/GeneNameQuery.py
+++ api_v0/GeneNameQuery.py
@@ -5,6 +5,7 @@ import requests
 import re
 import json
 
+from AtsnpExceptions import *
 from GenomicLocationQuery import GenomicLocationQuery
 from ElasticsearchURL import ElasticsearchURL, ElasticsearchResult
 
