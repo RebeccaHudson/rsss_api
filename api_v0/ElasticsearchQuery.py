fbebe99824e759539df710643144644edc046b20
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-12-25 21:35:46 +0000

    Add a default condition that narrows results for queries with only pvalue_rank specified into those that meet conditions for gain or loss of function.

--- api_v0/ElasticsearchQuery.py
+++ api_v0/ElasticsearchQuery.py
@@ -18,6 +18,9 @@ class ElasticsearchAtsnpQuery(object):
     #possible to put the Pvalue stuff into its own class?
     def setup_pvalue_filter(self):
         pvd = self.get_pvalue_dict()
+        if len(pvd.keys()) == 1:
+            print "detected DEFAULT mode p-value rank is the only one specified."
+            return self.prepare_json_for_pvalue_filter_default(pvd)
         return self.prepare_json_for_pvalue_filter_directional(pvd)
 
     #TODO: find out if > 1 pvalue is required.
@@ -38,8 +41,6 @@ class ElasticsearchAtsnpQuery(object):
         if not pv_dict:
             pv_dict['pvalue_rank'] = settings.DEFAULT_P_VALUE
         return pv_dict     
-
-
 
     #Don't require less zero if the direction is < 
     def fix_lte_for_zero_pvalues(self, operator, pvalue):
@@ -71,38 +72,109 @@ class ElasticsearchAtsnpQuery(object):
                        pvalue_name_in_index : { direction : str(pvalue) }
                     }
                 })
-         
-       #if 'pvalue_rank' in pvalue_dict:
-       #    dict_for_filter['filter'].append({ 
-       #    "range" : {
-       #        "pval_rank": { "lte":  str(pvalue_dict['pvalue_rank']) }
-       #              }
-       #    })
-       #
-       #if 'pvalue_ref' in  pvalue_dict:
-       #    pvalue_ref_direction = pvalue_dict['pvalue_ref_direction']
-       #    if pvalue_ref_direction == 'lt' and pvalue_dict['pvalue_ref'] == 0:
-       #        pvalue_ref_direction = 'lte' 
-       #    #either lte or gte
-       #    dict_for_filter['filter'].append({
-       #        "range" : {
-       #            "pval_ref": {
-       #                pvalue_ref_direction:  str(pvalue_dict['pvalue_ref']) 
-       #            }
-       #        }
-       #    })
-       #if 'pvalue_snp' in pvalue_dict:  
-       #    pvalue_snp_direction = pvalue_dict['pvalue_snp_direction']
-       #    if pvalue_snp_direction == 'lt' and pvalue_dict['pvalue_snp'] == 0:
-       #        pvalue_snp_direction = 'lte' #don't exclude records w/ pvalue = 0
-       #    dict_for_filter['filter'].append({
-       #    "range" : {
-       #        "pval_snp": {
-       #            pvalue_snp_direction:  str(pvalue_dict['pvalue_snp']) 
-       #                   }
-       #              }
-       #    })
        return dict_for_filter 
+
+
+
+
+    def setup_range_clause(self, pvalue, clause_info):
+        pv = str(clause_info['cutoff'])
+        operator = clause_info["operator"] 
+        clause = { "range" : { pvalue : { operator: pv } } }  
+        return clause
+
+    def setup_one_pval_case(self, gain_or_loss): 
+        which_case = settings.GAIN_AND_LOSS_DEFS[gain_or_loss]
+        one_case = { "bool":{ "must" : [] } }
+        for one_pval in [ "pval_ref", "pval_snp"]:
+            clause_info = which_case[one_pval]
+            rc = self.setup_range_clause(one_pval, clause_info) 
+            one_case["bool"]["must"].append(rc)
+        return one_case
+
+    #SHOULD BE REFACTORED.
+    #(The terms direction and operator are used synonomously here)
+    def prepare_json_for_pvalue_filter_default(self, pvalue_dict):
+       #pvalue_snp is missing at this point..
+       dict_for_filter = { "filter" : [] }
+       dict_for_filter['filter'].append({
+           "range" : {
+               'pval_rank' : { 'lte': str(pvalue_dict['pvalue_rank']) }
+            }
+       })
+       compound_filter = {"bool": {"minimum_should_match" : 1, "should":[] }}
+       gain_condition = self.setup_one_pval_case("gain")
+       compound_filter["bool"]["should"].append(gain_condition)  
+       #compound_filter["bool"]["should"].append(
+       #  { "bool" : { "must" : [
+       #                        { "range": { "pval_ref": { "lte": "0.002" } } },
+       #                        { "range": { "pval_snp": { "lte": "0.002" } } }
+       #                      ]
+       #             }
+       #  })
+
+       loss_condition = self.setup_one_pval_case("loss")
+       compound_filter["bool"]["should"].append(loss_condition)  
+       #compound_filter["bool"]["should"].append(
+       #  { "bool" : { "must" : [
+       #                        { "range": { "pval_ref": { "gte": "0.002" } } },
+       #                        { "range": { "pval_snp": { "gte": "0.002" } } }
+       #                      ]
+       #             }
+       #  })
+       dict_for_filter["filter"].append(compound_filter) 
+       return dict_for_filter 
+ 
+  
+
+
+
+       #pvalue rank always gets less than or equal to.
+       for one_pv in ['rank', 'ref', 'snp']:
+           pvalue_name = '_'.join(['pvalue', one_pv])
+           direction = None
+           if pvalue_name in pvalue_dict: 
+               pvalue = pvalue_dict[pvalue_name]
+               if one_pv == 'rank':
+                   direction = 'lte'
+               else: 
+                   which_direction = '_'.join([pvalue_name, 'direction'])
+                   direction = \
+                    self.fix_lte_for_zero_pvalues(pvalue_dict[which_direction],
+                                                  pvalue)
+               pvalue_name_in_index = '_'.join(['pval', one_pv])
+               dict_for_filter['filter'].append({
+                   "range" : {
+                       pvalue_name_in_index : { direction : str(pvalue) }
+                    }
+                })
+       return dict_for_filter 
+
+
+
+
+
+
+       #For query with extra conditions added: 
+       #try to use this twice -> 
+   
+
+       #in setup_pvalue_filter, check the p-values for the condition where:
+       #     p-value rank is set and the other 2 are not. 
+       #     if this condition is present:
+       #         create 2 pvalue-dicts based on the one that was provided by self.get_pvalue_dict()
+       #         use prepare_json_for_pvalue_filter_directional on each of them. 
+       #         bool them together using proper Elasticsearch query syntax.
+       #prepare 2 p-value filters, 'or' them.
+       #     if this condition is not present:
+       #       do exactly what has been done up until now.
+
+
+
+
+
+
+
 
     #For sort, 'coordinate' means 'chr' and 'pos'
     #Replace coordinate with the fields chr and pos, in that order.
@@ -131,6 +203,7 @@ class ElasticsearchAtsnpQuery(object):
             params[one_key] = self.request.data.get(one_key)     
         return params
 
+    #This is the format we want the compound DEFAULT query to fit into.
     def elasticsearch_base_query(self, query_data, pvalue_filter):
         q = {"query":
               {"bool": 
